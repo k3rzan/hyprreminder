f47ca40d42cbe34af74ce412a8e83f1615d86ed9
Kerzan <kerzan.dev@gmail.com>
2025-05-11 19:23:31 -0500

    feat: create Category type
    This type of basically a list of Notes. This is done by using arrays of
    strings and creating the Notes with some math to make them render one
    on top of the other but with specific spacing to make them look nice.

--- src/main.zig
+++ src/main.zig
@@ -13,16 +13,17 @@ pub fn main() anyerror!void {
 
     const text = ui.Font{
         .color = .dark_gray,
-        .size = 16,
-        .value = "This is a note",
+        .size = 24,
+        .value = "This is a title",
     };
 
-    const note = ui.Note().init(
-        text,
-        .{ .x = ui.base_size, .y = ui.base_size * 4 },
-        .{ .h = ui.base_size * 5, .w = screenWidth - ui.base_size * 2 },
-        .gray,
-    );
+    var notes = [_][:0]const u8{
+        "this is a test",
+        "this is another test",
+        "this is another test",
+        "this is another test",
+    };
+    const category = try ui.Category().init(notes[0..], text, .{ .x = ui.base_size, .y = ui.base_size * 5 });
 
     while (!rl.windowShouldClose()) {
         rl.beginDrawing();
@@ -30,8 +31,8 @@ pub fn main() anyerror!void {
 
         rl.clearBackground(rl.Color.light_gray);
 
-        rl.drawText("hello world", ui.base_size, ui.base_size, ui.base_size * 2.0, .gray);
+        rl.drawText("hello world", ui.base_size, ui.base_size, ui.base_size * 3.0, .gray);
 
-        note.render();
+        category.render();
     }
 }
--- src/ui.zig
+++ src/ui.zig
@@ -1,4 +1,5 @@
 const rl = @import("raylib");
+const std = @import("std");
 
 pub const scale = 1.0;
 pub const base_size: f32 = 16.0;
@@ -35,6 +36,55 @@ pub fn Note() type {
                 .position = position,
                 .size = size,
                 .bg_color = bg_color,
+            };
+        }
+    };
+}
+
+pub fn Category() type {
+    return struct {
+        const Self = @This();
+        notes: []Note(),
+        title: Font,
+        allocator: std.mem.Allocator,
+        position: Position,
+
+        pub fn render(self: Self) void {
+            const title_size: f32 = @floatFromInt(self.title.size);
+            rl.drawText(self.title.value, @intFromFloat(self.position.x), @intFromFloat(self.position.y), base_size * 2, .dark_gray);
+            for (self.notes, 0..) |*note, index| {
+                const i: f32 = @floatFromInt(index);
+                note.size = .{ .h = base_size * 5.0, .w = base_size * 20 };
+                note.position = .{ .x = self.position.x, .y = self.position.y + title_size + (i * (@as(f32, @floatFromInt(note.size.h)) + base_size)) + base_size * 1.5 };
+                note.render();
+            }
+        }
+
+        pub fn deinit(_: Self) void {
+            std.debug.print("deinited\n", .{});
+        }
+
+        pub fn init(note_values: [][:0]const u8, title: Font, position: Position) !Self {
+            var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+            const allocator = arena.allocator();
+            var notes = try std.ArrayListUnmanaged(Note()).initCapacity(allocator, note_values.len);
+
+            for (note_values) |value| {
+                try notes.append(
+                    allocator,
+                    .{
+                        .text = .{ .color = .dark_gray, .size = base_size, .value = value },
+                        .bg_color = .gray,
+                        .position = .{ .x = 0.0, .y = 0.0 },
+                        .size = .{ .h = 0.0, .w = 0.0 },
+                    },
+                );
+            }
+            return Self{
+                .title = title,
+                .notes = notes.items,
+                .allocator = allocator,
+                .position = position,
             };
         }
     };
